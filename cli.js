e1a4685949e4684a7f67ac6b64334cbd0e36f213
Kevin <github@magicmonkey.org>
2014-07-06 16:18:24 +0100

    Fixed the single-L8 examples

--- cli.js
+++ cli.js
@@ -2,10 +2,18 @@ var l8 = require('./l8');
 var util = require('util');
 var SerialPortStream = require('serial-port-stream');
 
+var l8_pics = require('./l8_pics');
+
 var streama = new SerialPortStream('/dev/rfcomm0');
 var l8a = l8.init(streama);
-var streamb = new SerialPortStream('/dev/rfcomm0');
-var l8b = l8.init(streamb);
+
+l8a
+.on('packet', function(pkt) {
+	//console.log("A", pkt);
+})
+.on("VOLTAGE_RESPONSE", function(pkt) {
+	console.log("L8 A has a battery level of " + pkt.level + "%");
+});
 
 streama.on('open', function() {
 
@@ -20,13 +28,16 @@ streama.on('open', function() {
 	}
 	//l8a.send('MATRIX_SET', {pixels:pixels});
 
+	setInterval(function() {
+		l8a.send('VOLTAGE_QUERY');
+	}, 1000);
 });
 
 var stdin = process.openStdin();
 process.stdin.setRawMode(true);
 process.stdin.resume();
 stdin.on('data', function (key) {
-	console.log(key);
+	//console.log(key);
 
 	switch (key[0]) {
 
@@ -40,6 +51,18 @@ stdin.on('data', function (key) {
 				pixels[i] = {b:(i%2), g:(i%4), r:(i%8)};
 			}
 			l8a.send('MATRIX_SET', {pixels:pixels});
+			break;
+
+		case 0x33:
+			l8a.send('VOLTAGE_QUERY');
+			break;
+
+		case 0x34:
+			l8a.send('MATRIX_SET', {pixels:l8_pics.mail});
+			break;
+
+		case 0x35:
+			l8a.send('MATRIX_SET', {pixels:l8_pics.boxes});
 			break;
 
 		case 0x03: // ctrl-c
